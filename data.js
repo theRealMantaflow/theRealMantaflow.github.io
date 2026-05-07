17821682b6d689dd246540596e164e2d9b86713e
Sreeram <iamsreerams@gmail.com>
2026-05-07 19:12:30 +0530

    Full revamp

--- data.js
+++ data.js
@@ -1,43 +1,185 @@
 const data = {
-    name: "Sreeram Sreekumar Nair",
+    name: "Sreeram S Nair",
 
-    about: "Software Engineer & ML Engineer focused on high-performance systems and GenAI.",
+    about: `Software Engineer and Machine Learning Engineer with one year of research experience. 
+    Designed and developed a proof-of-concept model for path finding, that improved benchmark performance by 24% over traditional methods such as A star. <br>
+    Proficient in Python and C++, with practical experience in Machine Learning, software development, interface design with Qt6, and PostgreSQL. <br>
+    Analytical, detail-oriented, and effective in collaborative environments.`,
 
     contact: `
         📧 iamsreerams@gmail.com |
-        <a href="https://github.com/theRealMantaflow">GitHub</a>
+        <svg height="16" width="16" viewBox="0 0 16 16" fill="currentColor" style="vertical-align: middle; margin-right: 4px;">
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 
+            7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49
+            -2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82
+            -1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58
+            1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87
+            .51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31
+            -1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0
+            .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09
+            2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16
+            1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07
+            -1.87 3.75-3.65 3.95.29.25.54.73.54 1.48
+            0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01
+            8.01 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+        </svg><a href="https://github.com/theRealMantaflow" target="_blank">GitHub</a> |
+        
+        <svg height="16" width="16" viewBox="0 0 16 16" fill="currentColor" style="vertical-align: middle; margin-right: 4px;">
+             <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 
+             16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 
+             15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 
+             1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 
+             3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 
+             1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 
+             0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+        </svg><a href="https://www.linkedin.com/in/realmantaflow/" target="_blank">LinkedIn</a>
     `,
 
+    copyrights: `
+        (c) All rights to the character(s) in the backdrop reserved to HoYoverse.
+    `,
+
+    credits_phone: `Artwork in the backdrop by <a href="https://dribbble.com/FukoKaichou" target="_blank">Stefany Pauline Moyco</a>.`,
+
+    // // path | credits
+    backgrounds: [
+        "assets/bg.png | Backdrop belongs to the artist, can be removed as per request.",
+        "assets/bg2.jpeg | Backdrop belongs to the artist, can be removed as per request.",
+        "assets/bg3.jpg | Backdrop belongs to the artist, can be removed as per request.",
+        'assets/bg4_downscale.png | Artwork in the backdrop by <a href="https://www.pixiv.net/en/artworks/113950140" target="_blank">Lik/极夜繁声</a>.'
+    ],
+
+    // path | credits
+    phone_backgrounds: [
+        `assets/bg_phone.webp | Artwork in the backdrop by <a href="https://dribbble.com/FukoKaichou" target="_blank">Stefany Pauline Moyco</a>.`,
+        "assets/bg_phone2.png | Backdrop belongs to the artist, can be removed as per request.",
+        "assets/bg_phone3.png | Backdrop belongs to the artist, can be removed as per request.",
+        `assets/bg_phone4.jpg | Artwork in the backdrop by <a href="https://www.pixiv.net/en/artworks/109668059" target="_blank">Lik/极夜繁声</a>.`,
+        `assets/bg_phone5.png | Artwork in the backdrop by <a href="https://www.pixiv.net/en/artworks/109668167" target="_blank">Lik/极夜繁声</a>.`,
+        `assets/bg_phone6.jpg | Artwork in the backdrop by <a href="https://www.pixiv.net/en/artworks/109668112" target="_blank">Lik/极夜繁声</a>.`
+    ],
+
     skills: [
-        "C++", "Python", "PyTorch",
-        "Qt6", "PostgreSQL"
+        {
+            "Programming Languages" : "C, C++, Python",
+            "Frameworks" : "PyTorch, Qt6, Flask",
+            "Database Engine" : "PostgreSQL",
+            "CAD" : "Altium, PCB Design"
+        },
+    ],
+
+    work_experience: [
+        {
+            title: "Research Intern",
+            company: "Singapore University of Technology and Design, Singapore",
+            duration: "February 2024 - March 2024",
+            description: `
+                <ul>
+                    <li>Interfaced a Husky-lens AI camera for gesture-based robot control, successfully deployed in a children’s AI competition. 
+                    Made use of the ESP-NOW protocol for communication between Husky-lens and the robot.</li>
+                    <li>Contributed to the curriculum for shape-changing robots (Smorphi) at Wefaa Robotics.</li>
+                    <li>Visited the Wefaa Robotics HQ and took part in the stock taking and QA process.</li>
+                </ul>
+            `
+        },
+        {
+            title: "Research Intern",
+            company: "Humanitarian Technology Labs, Kollam, Kerala, India",
+            duration: "June 2024 - June 2025",
+            description: `
+                <ul>
+                    <li>Dataset & GenAI Model: Engineered a synthetic dataset of 2D Maps and designed a GAN model in PyTorch.</li>
+                    <li>Achievement: Achieved 24% improvement in computation efficiency (best-case scenario).</li>
+                    <li>Paper: Co-authored a paper on GenAI-based path planning, in collaboration with Singapore University of Technology and Design.</li>
+                </ul>
+            `
+        }
     ],
 
     projects: [
         {
-            title: "Chess Engine",
+            title: "Inventory & Billing System: Vinventory",
+            date: "March 2026",
             description: `
-                <p><b>Modern C++ engine</b> using bitboards.</p>
+                <p>Inventory and billing system for Linux, with Qt6 frontend + PostgreSQL backend.</p>
                 <ul>
-                    <li>Memory efficient</li>
-                    <li>Extensible move generation</li>
+                    <li>Developed an inventory and billing software for Linux desktop environments, using Qt6 for the frontend and PostgreSQL for the backend.</li>
+                    <li>Used PostgreSQL as database engine, to enable multiple billing kiosks at the same time.</li>
+                    <li>Simple user interface to enable easy adoption even for clients with relatively low computer literacy.</li>
+                    <li>C++ uses minimal system resources, ensuring compatibility with legacy desktop environments running Linux.</li>
                 </ul>
+                <p>Github link is set to private, since the project has market potential.</p>
             `,
             images: [
-                "assets/projects/chess.png",
-                "assets/projects/chess.png"
+                "assets/projects/vinventory/billing.png",
+                "assets/projects/vinventory/vinventory_er.png",
+                "assets/projects/vinventory/categories_uom.png",
+                "assets/projects/vinventory/hsntax.png"
             ]
         },
 
         {
-            title: "Inventory & Billing System",
+            title: "Chess Engine",
+            date: "January 2026",
             description: `
-                <p>Qt6 frontend + PostgreSQL backend.</p>
-                <p><i>Supports multiple kiosks.</i></p>
+                <p><b>Modern C++ engine</b> using bitboards.</p>
+                <ul>
+                    <li>Developed a high-performance chess engine from scratch using Modern C++.</li>
+                    <li>Used the concept of Bitboards for efficient memory representation.</li>
+                    <li>Capable of interfacing with any move-generation algorithm.</li>
+                </ul>
+                <p>Github link: <a href="https://github.com/theRealMantaflow/Chess_Engine" target="_blank">Chess Engine</a></p>
             `,
             images: [
-                "assets/projects/billing.png"
+                // "assets/projects/chess.png",
+                // "assets/projects/chess.png"
             ]
+        },
+
+        {
+            title: "LED Chaser on Custom PCB",
+            date: "June 2025",
+            description: `
+                <p>LED chaser circuit on a custom PCB.</p>
+                <ul>
+                    <li>Designed a heart-shaped PCB using Altium and fabricated the hardware on JLC PCB.</li>
+                    <li>The circuit was successfully simulated using tinkercad.</li>
+                    <li>Implemented timing logic using 555 timers and validated the circuit through soldering and hardware debugging.</li>
+                    <li>A mistake was made during the design of the PCB. This resulted in a shift register getting fried.</li>
+                    <li>Project had to be abandoned due to budget constraints.</li>
+                </ul>
+                <p>TinkerCAD simulation links:</p>
+                <ul>
+                    <li><a href="https://www.tinkercad.com/things/h6gB8uJq0Vs-8-led-chaser?sharecode=oLhxr44sHEzSr3g1d2qc7OvELaihwcHcSUFkKrTk_yw" target="_blank">8 LED Chaser</a></li>
+                    <li><a href="https://www.tinkercad.com/things/dXm5E9AIWAT-bjt-16-led-chaser?sharecode=1yBNKGxQJgIsQG1vqw6ErRM5bbFMc9ulWsKBG9Q2gIA" target="_blank">16 LED Chaser with BJT, derived from 8 LED Chaser</a></li>
+                </ul>
+            `,
+            images: [
+                "assets/projects/led_chaser/pcb_1.png",
+                "assets/projects/led_chaser/pcb_2.png",
+                "assets/projects/led_chaser/sheet_1.png",
+                "assets/projects/led_chaser/sheet_2.png",
+                "assets/projects/led_chaser/sheet_3.png"
+            ]
+        }
+    ],
+
+    education: [
+        {
+            institution: "Amrita Vishwa Vidyapeetham",
+            degree: "B.Tech in Electronics and Computer Engineering",
+            location: "Kollam, Kerala, India",
+            duration: "2021 - 2025",
+            description: `
+            Coursework: DSA, Machine Learning, Computer Architecture, Operating Systems, Embedded Systems.<br>
+            Grade: First Class with Distinction (8.32 CGPA)<br>
+            Notable Activities: As part of Student Social Responsibility program, took cyber awareness classes (Beyond the Blueprint) 
+            for high school students. Received great feedback from their parents.<br>
+            Achievements:<ul>
+                <li>Won second place in the Transformer Robot Soccer League (2023), hosted by RoboRoarz Singapore.</li>
+                <li>Won third place at the Smorphi Imaginary Robotics Challenge (2024) and spotted a bug in their platform.</li>
+            </ul>
+            `
         }
     ]
 };
